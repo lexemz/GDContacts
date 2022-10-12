652193e12a7e5c2e12cfbea2f1a9b02880fd4774
Igor Buzykin <lexemz@gmail.com>
2022-10-12 16:38:46 +0300

    Add activity indicators on ContactsListViewController

--- GreenData-iOS-Case/ViewControllers/ContactsListViewController.swift
+++ GreenData-iOS-Case/ViewControllers/ContactsListViewController.swift
@@ -7,24 +7,38 @@
 
 import UIKit
 
-class ContactsListViewController: UITableViewController {
+class ContactsListViewController: UIViewController {
+  @IBOutlet var tableView: UITableView!
+  @IBOutlet var mainActivityIndicator: UIActivityIndicatorView!
+
   // MARK: - Private props
+
+  // Managers
   private let contactsManager = ContactsManager()
-  
+
+  // Flow Properties
   private var contacts: [ContactRepresentable] = []
-  
+
   // MARK: - VC lifecycle methods
 
   override func viewDidLoad() {
     super.viewDidLoad()
-    contactsManager.delegate = self
+
     configureTableView()
-    contactsManager.fetchContacts()
+    configureContactsManager()
   }
-  
+
   // MARK: - Flow methods
-  
+
+  private func openContantViewController(with contact: ContactRepresentable) {
+    let contactVC = ContactDetailViewController.instanceFromStoryboard()
+    contactVC.contact = contact
+    navigationController?.pushViewController(contactVC, animated: true)
+  }
+
   private func configureTableView() {
+    tableView.delegate = self
+    tableView.dataSource = self
     tableView.register(
       ContactViewCell.self,
       forCellReuseIdentifier: ContactViewCell.reuseIdentifier
@@ -32,54 +46,67 @@ class ContactsListViewController: UITableViewController {
     tableView.rowHeight = 85
   }
 
-  private func openContantViewController(with contact: ContactRepresentable) {
-    let contactVC = ContactDetailViewController.instanceFromStoryboard()
-    contactVC.contact = contact
-    navigationController?.pushViewController(contactVC, animated: true)
-  }
-  
-  private func fetchContactsFromManager() {
+  private func configureContactsManager() {
+    contactsManager.delegate = self
     contactsManager.fetchContacts()
+    showActivityIndicator()
   }
-  
-  // MARK: - TableViewDataSource
-  override func tableView(
+
+  private func showActivityIndicator() {
+    mainActivityIndicator.startAnimating()
+  }
+
+  private func hideActivityIndicator() {
+    mainActivityIndicator.stopAnimating()
+  }
+}
+
+// MARK: - TableViewDataSource
+
+extension ContactsListViewController: UITableViewDataSource {
+  func tableView(
     _ tableView: UITableView,
     numberOfRowsInSection section: Int
   ) -> Int {
     contacts.count
   }
 
-  override func tableView(
+  func tableView(
     _ tableView: UITableView,
     cellForRowAt indexPath: IndexPath
   ) -> UITableViewCell {
     let cell = tableView.dequeueReusableCell(
       withIdentifier: ContactViewCell.reuseIdentifier
     ) as! ContactViewCell
-    
+
     cell.configure(with: contacts[indexPath.row])
-    
-    if indexPath.row == contacts.count - 3 {
-      loadMoreContacts()
+
+    if indexPath.row == contacts.count - 1 {
+      contactsManager.fetchContacts()
     }
     return cell
   }
-  
-  // MARK: - TableViewDelegate
-  
-  override func tableView(
+}
+
+// MARK: - TableViewDelegate
+
+extension ContactsListViewController: UITableViewDelegate {
+  func tableView(
     _ tableView: UITableView,
     didSelectRowAt indexPath: IndexPath
   ) {
     tableView.deselectRow(at: indexPath, animated: true)
     openContantViewController(with: contacts[indexPath.row])
   }
-  
-  // MARK: - Flow methods
-  
-  private func loadMoreContacts() {
-    contactsManager.fetchContacts()
+
+  func tableView(_ tableView: UITableView, willDisplay cell: UITableViewCell, forRowAt indexPath: IndexPath) {
+    let lastRowIndex = tableView.numberOfRows(inSection: 0) - 1
+    if indexPath.row == lastRowIndex {
+      let activity = UIActivityIndicatorView()
+      activity.frame = CGRect(x: 0, y: 0, width: tableView.bounds.width, height: 40)
+      activity.startAnimating()
+      tableView.tableFooterView = activity
+    }
   }
 }
 
@@ -94,14 +121,16 @@ extension ContactsListViewController: ContactsManagerDelegate {
     DispatchQueue.main.async {
       self.contacts += contacts
       self.tableView.reloadData()
+      self.hideActivityIndicator()
     }
   }
-  
+
   func contactsManager(
     _ contactsManager: ContactsManager,
     didReceive error: NetworkManagerError
   ) {
     // TODO: Show Alert
+    tableView.tableFooterView?.isHidden = true
     Log.d(error)
   }
 }
