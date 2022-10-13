86958fc10c75be9ed85cd2e7a7ab9226e39c4016
Igor Buzykin <lexemz@gmail.com>
2022-10-13 22:58:32 +0300

    Update UIViewController extensions

--- GreenData-iOS-Case/Extensions/UIViewController+Extensions.swift
+++ GreenData-iOS-Case/Extensions/UIViewController+Extensions.swift
@@ -14,4 +14,15 @@ extension UIViewController {
       withIdentifier: String(describing: self)
     ) as! Self
   }
+  
+  func showInfoAlert(title: String?, message: String?) {
+    let alert = UIAlertController(
+      title: title,
+      message: message,
+      preferredStyle: .alert
+    )
+    let ok = UIAlertAction(title: "OK", style: .default)
+    alert.addAction(ok)
+    present(alert, animated: true)
+  }
 }
