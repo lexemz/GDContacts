734e9c4037267a45e9dd83cd1dc6ae0fed2ef325
Igor Buzykin <lexemz@gmail.com>
2022-10-14 14:29:23 +0300

    Update ContactDetailViewController

--- GreenData-iOS-Case/ViewControllers/ContactDetailViewController.swift
+++ GreenData-iOS-Case/ViewControllers/ContactDetailViewController.swift
@@ -10,44 +10,72 @@ import SimpleImageViewer
 import UIKit
 
 final class ContactDetailViewController: UIViewController {
+  // MARK: - IBOutlets
+
   @IBOutlet var contactImageView: UIImageView!
   @IBOutlet var birthdateLabel: UILabel!
-  @IBOutlet var localTimeLabel: UILabel!
-  @IBOutlet var userTimeLabel: UILabel!
+  @IBOutlet var deviceTimeLabel: UILabel!
+  @IBOutlet var contactTimeLabel: UILabel!
   @IBOutlet var numberLabel: UILabel!
   @IBOutlet var mailLabel: UILabel!
   
   @IBOutlet var genderImageView: UIImageView!
   @IBOutlet var genderLabel: UILabel!
   
+  // MARK: - Internal properties
+
   var contact: Contact!
-  let dateManager = DateManager()
+  
+  // MARK: - Private properties
+
+  private let dateManager = DateManager()
+  
+  // MARK: - VC lifecycle methods
   
   override func viewDidLoad() {
     super.viewDidLoad()
     configureUI()
-    setupGestureRecognizers()
+    configureGestureRecognizers()
   }
+  
+  // MARK: - Flow private methods
   
   private func configureUI() {
     title = contact.fullname
-    
+    configureLabels()
+    configureGenderInfo()
+    configureImageView()
+    configureTime()
+  }
+  
+  private func configureLabels() {
     let birthDate = dateManager.parseISO8601Date(string: contact.birthdayDate) ?? "N/A"
     birthdateLabel.text = "\(birthDate) (\(contact.birthdayAge))"
     numberLabel.text = contact.phoneNumber
     mailLabel.text = contact.mail
-    
-    userTimeLabel.text = "Наше: \(dateManager.getCurrentTime())"
-    let localTime = dateManager.getTimeByTimezone(timezone: contact.localTimeOffset) ?? "N/A"
-    localTimeLabel.text = "Местное \(localTime)"
+  }
+  
+  private func configureTime() {
+    dateManager.startTrackingLiveTime(for: contact.localTimeOffset) {
+      [weak self] contactTime, deviceTime in
+      guard let self = self else { return }
+      self.contactTimeLabel.text = "Местное: \(contactTime)"
+      self.deviceTimeLabel.text = "Наше: \(deviceTime)"
+    }
+  }
+  
+  private func configureGenderInfo() {
     genderLabel.text = contact.gender.presentTitle
     genderImageView.image = UIImage(named: contact.gender.imageName)
+  }
+  
+  private func configureImageView() {
     contactImageView.kf.setImage(with: URL(string: contact.picURL))
     contactImageView.layer.cornerRadius = 10
     contactImageView.layer.masksToBounds = true
   }
   
-  private func setupGestureRecognizers() {
+  private func configureGestureRecognizers() {
     let tapGestureRecognizer = UITapGestureRecognizer(
       target: self,
       action: #selector(imageViewTapDetected)
